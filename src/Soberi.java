91acb5f3889daed362b14c70226c2c5b1cb1d69f
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 13:11:06 +0100

    Proekt22

---
+++ src/Soberi.java
@@ -0,0 +1,11 @@
+
+public class Soberi {
+
+	public static void main(String[] args) {
+		int sum = Soberibroevi(2, 3, 4);
+		System.out.println("Sumata na triete broja = " + sum);
+	}
+public static int Soberibroevi(int a, int b, int c){
+	return (a+b+c);
+}
+}
